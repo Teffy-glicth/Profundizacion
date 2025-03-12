47ab03d5bdf34df0e093af363bccd72b6093ffa5
Trabajo <esteffy.bc@gmail.com>
2025-03-12 18:19:33 -0500

    Forzando Carpeta .venv

---
+++ .venv/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+import re
+from linearRegression import calculateGrade
+from linearRegression import grafica
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+
+    match_object = re.fullmatch("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "friend"
+
+    content = f"Hello there, {clean_name} ! Hour: {now}"
+    return content
+
+@app.route("/example/")
+def exampleHTML():
+    return render_template("example.html")  
+
+@app.route("/linearRegression/", methods =["GET", "POST"])
+def linearRegression () :
+    calculateResult = None
+    plot_url = None
+    if request.method == "POST":
+        hours = float (request.form["hours"])
+        calculateResult = calculateGrade(hours)
+        plot_url= grafica(hours, calculateResult)
+
+    return render_template("linearRegressionGrades.html", result = calculateResult, plot_url= plot_url)
