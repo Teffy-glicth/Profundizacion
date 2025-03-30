543efd152dc9577415683a8fa52547c561b6a947
SantiagoTH0 <davidsantiagotorres06@gmail.com>
2025-03-30 00:51:22 -0500

    Revert "Modelo de regresion logistica"

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 import re
-import numpy as np
 from linearRegression import calculateGrade
 from linearRegression import grafica
-from LogisticRegression import encoder_Party, encoder_Region, scaler, model, generate_plot
 
 app = Flask(__name__)
 
@@ -44,29 +42,3 @@ def linearRegression () :
 @app.route("/mindmeister/")
 def mindmeisterHTML():
     return render_template("mindmeister.html")
-
-@app.route("/LogisticRegression/", methods =["GET", "POST"])
-def LogisticRegression () :
-    result = None
-    plot_url = None
-
-    if request.method == "POST":
-        try:
-            age = int(request.form ["Age"])
-            region = request.form ["Region"]
-            previous_answer = request.form ["Previous_Answer"]
-            income = float (request.form ["Income"])
-
-            region_encoded =encoder_Region.transform ([[region]]).toarray()
-            party_encoded = encoder_Party.transform([[previous_answer]]).toarray()
-            income_scaled = scaler.transform([[income]])
-
-            input_data = np.hstack ([[age], income_scaled.flatten(), region_encoded.flatten(), party_encoded.flatten()]).reshape (1, -1)
-            result = model.predict (input_data)[0]
-            plot_url =generate_plot()
-
-        except Exception as e:
-            print ("Error: ", e)
-
-    return render_template("LogisticRegression.html", result = result, plot_url= plot_url)
-
