e5aa3b9dc8ce63279b7f5035671a8c6aa10df44b
Teffy-glicth <esteffy.bc@gmail.com>
2025-05-11 04:16:51 +0000

    Base de datos

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://modelos_db_user:lZUg1NORsgAtBIrBJR70jWwcOUldbsow@dpg-cvq1ngjuibrs73879030-a.oregon-postgres.render.com/modelos_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:jqQOlKNFIZIVBtzpfWccqomoTmiXrjdU@trolley.proxy.rlwy.net:15444/railway'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- insertar_datos.py
+++ insertar_datos.py
@@ -5,14 +5,11 @@ from models import Modelo
 with app.app_context():
     nuevo_modelo = Modelo(
         nombre="Naive Bayes",
-        descripcion="""El Naive Bayes es un algoritmo de aprendizaje supervisado basado en el Teorema de Bayes, utilizado principalmente para clasificación. Funciona calculando la probabilidad de que un dato pertenezca a una clase específica, utilizando la suposición de independencia condicional entre características (de ahí el término "ingenuo"), lo que simplifica los cálculos al considerar que cada atributo contribuye de manera independiente a la probabilidad final 
-        Características: 
-        - Simplicidad: Fácil de implementar y entender, ideal para proyectos rápidos o como punto de referencia comparativo. 
-        - Eficiencia con grandes volúmenes de datos: Procesa rápidamente conjuntos masivos, como textos o registros médicos, sin requerir alta capacidad computacional. 
-        - Rendimiento en datos categóricos: Destaca en aplicaciones con variables discretas (ejemplo: presencia de palabras en correos electrónicos). 
-        - Robustez ante características irrelevantes: Mitiga el impacto de variables poco informativas en las predicciones finales. Tipos de modelos: Incluye variantes como Gaussiano (para datos continuos), Multinomial (conteos de frecuencias) y Bernoulli (datos binarios). 
-        - Limitaciones: La suposición de independencia entre características puede reducir precisión en problemas complejos, y enfrenta problemas con categorías no vistas en entrenamiento (requiere técnicas como alisamiento). 
-        - Aplicaciones comunes: Clasificación de documentos, sistemas de recomendación, detección de spam y análisis predictivo en medicina""",
+        descripcion="""El Naive Bayes es un algoritmo de aprendizaje supervisado basado en el Teorema de Bayes, utilizado principalmente para clasificación. Funciona calculando la probabilidad de que un dato pertenezca a una clase específica, utilizando la suposición de independencia condicional entre características (de ahí el término "ingenuo"), lo que simplifica los cálculos al considerar que cada atributo contribuye de manera independiente a la probabilidad final Características: 
+        Simplicidad: Fácil de implementar y entender, ideal para proyectos rápidos o como punto de referencia comparativo. Eficiencia con grandes volúmenes de datos: Procesa rápidamente conjuntos masivos, como textos o registros médicos, sin requerir alta capacidad computacional. 
+        Rendimiento en datos categóricos: Destaca en aplicaciones con variables discretas (ejemplo: presencia de palabras en correos electrónicos). Robustez ante características irrelevantes: Mitiga el impacto de variables poco informativas en las predicciones finales. 
+        Tipos de modelos: Incluye variantes como Gaussiano (para datos continuos), Multinomial (conteos de frecuencias) y Bernoulli (datos binarios). Limitaciones: La suposición de independencia entre características puede reducir precisión en problemas complejos, y enfrenta problemas con categorías no vistas en entrenamiento (requiere técnicas como alisamiento). 
+        Aplicaciones comunes: Clasificación de documentos, sistemas de recomendación, detección de spam y análisis predictivo en medicina""",
          fuente="""https://aprendeia.com/algoritmo-naive-bayes-machine-learning/ https://forum.huawei.com/enterprise/intl/es/m%C3%A9todo-supervisado-clasificaci%C3%B3n-naive-bayes/thread/872753-100757?isInitURL=true""",
         contenido_grafico="img/naivebayes.png"
     )
